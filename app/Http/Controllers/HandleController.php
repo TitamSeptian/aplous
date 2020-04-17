9d7d0019672b795dfa99a5cacd52a1459a695ca3
TitamSeptian <Itsmystyles9889@gmail.com>
2020-04-18 06:01:51 +0700

    show untung rugi

--- app/Http/Controllers/HandleController.php
+++ app/Http/Controllers/HandleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Log;
 use Auth;
+use DB;
 
 class HandleController extends Controller
 {
@@ -24,18 +25,26 @@ class HandleController extends Controller
 	public function dashboard()
 	{
 		$data = [];
-		$data['member'] = count(\App\Member::all());
-		$data['outlet'] = count(\App\Outlet::all());
-		$data['jenis'] = count(\App\Jenis::all());
-		$data['user'] = count(\App\TbUser::all());
+		$data['member'] = \App\Member::all();
+		$data['outlet'] = \App\Outlet::all();
+		$data['jenis'] = \App\Jenis::all();
+		$data['user'] = \App\TbUser::all();
 		if (Auth::user()->level == 'admin') {
-			$data['transaksi'] = count(\App\Transaksi::all());
-			$data['paket'] = count(\App\Paket::all());
+			$data['transaksi'] = \App\Transaksi::all();
+			$data['paket'] = \App\Paket::all();
 		}else{
-			$data['transaksi'] = count(\App\Transaksi::where('id_outlet', Auth::user()->tbUser->id_outlet)->get());
-			$data['paket'] = count(\App\Paket::where('id_outlet', Auth::user()->tbUser->id_outlet)->get());
+			$data['transaksi'] = \App\Transaksi::where('id_outlet', Auth::user()->tbUser->id_outlet)->get();
+			$data['paket'] = \App\Paket::where('id_outlet', Auth::user()->tbUser->id_outlet)->get();
 		}
-		// dd($data['member']);
+
+		$data['x'] = DB::table('tb_detail_transaksi')
+                    ->join('tb_paket', 'tb_detail_transaksi.id_paket', '=', 'tb_paket.id')
+                    ->select(DB::raw('SUM(tb_detail_transaksi.qty * tb_paket.harga) AS total'))
+                    ->get();
+        // $data['pajak'] = $pajak = $data['data']->pajak/100 * $data['a']->total;
+        // $data['diskon'] = $diskon = $data['data']->diskon/100 * $data['a']->total;
+        // $data['total'] = $data['a']->total + $pajak - $diskon + $data['data']->biaya_tambahan;
+        // dd($data['a']);
 
 		return view('dashboard', $data);
 	}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
             <div class="d-flex d-lg-flex d-md-block align-items-center">
                 <div>
                     <div class="d-inline-flex align-items-center">
-                        <h2 class="text-dark mb-1 font-weight-medium">{{ $member }}</h2>
+                        <h2 class="text-dark mb-1 font-weight-medium">{{ count($member) }}</h2>
                     </div>
                     <h6 class="text-muted font-weight-normal mb-0 w-100 text-truncate">Pelanggan</h6>
                 </div>
@@ -20,7 +20,7 @@
         <div class="card-body">
             <div class="d-flex d-lg-flex d-md-block align-items-center">
                 <div>
-                    <h2 class="text-dark mb-1 w-100 text-truncate font-weight-medium">{{ $transaksi }}</h2>
+                    <h2 class="text-dark mb-1 w-100 text-truncate font-weight-medium">{{ count($transaksi) }}</h2>
                     <h6 class="text-muted font-weight-normal mb-0 w-100 text-truncate">Cucian
                     </h6>
                 </div>
@@ -35,7 +35,7 @@
             <div class="d-flex d-lg-flex d-md-block align-items-center">
                 <div>
                     <div class="d-inline-flex align-items-center">
-                        <h2 class="text-dark mb-1 font-weight-medium">{{ $paket }}</h2>
+                        <h2 class="text-dark mb-1 font-weight-medium">{{ count($paket) }}</h2>
                     </div>
                     <h6 class="text-muted font-weight-normal mb-0 w-100 text-truncate">Paket </h6>
                 </div>
@@ -50,7 +50,7 @@
         <div class="card-body">
             <div class="d-flex d-lg-flex d-md-block align-items-center">
                 <div>
-                    <h2 class="text-dark mb-1 font-weight-medium">{{ $outlet }}</h2>
+                    <h2 class="text-dark mb-1 font-weight-medium">{{ count($outlet) }}</h2>
                     <h6 class="text-muted font-weight-normal mb-0 w-100 text-truncate">Toko</h6>
                 </div>
                 <div class="ml-auto mt-md-3 mt-lg-0">
@@ -61,5 +61,126 @@
     </div>
     @endif
 </div>
+@if(Auth::user()->level == 'admin')
+@foreach($outlet as $a)
+<div class="card">
+    <div class="card-body">
+        <div class="d-flex d-lg-flex d-md-block align-items-center">
+            <div>
+                <h3 class="text-dark mb-1 font-weight-medium">{{ $a->nama }}</h3>
+                <h6 class="text-muted font-weight-normal mb-0 w-100 text-truncate">{{ $a->alamat }}</h6>
+            </div>
+            <div class="ml-auto mt-md-3 mt-lg-0">
+                <span class="opacity-7 text-muted"><i class="fas fa-hospital-alt"></i></span>
+            </div>
+        </div>
+        <hr>
+        <table>
+            <tr>
+                <td width="100">Pemasukan</td>
+                <td width="100">Pengeluaran</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>
+                    @php
+                    $ts = App\Transaksi::where('id_outlet', $a->id)->get();
+                    $total = 0;
+                    foreach ($ts as $t) {
+                        $dt = DB::table('tb_detail_transaksi')
+                                ->join('tb_paket', 'tb_detail_transaksi.id_paket', '=', 'tb_paket.id')
+                                ->select(DB::raw('SUM(tb_detail_transaksi.qty * tb_paket.harga) AS total'))
+                                ->where('tb_detail_transaksi.id_transaksi', $t->id)
+                                ->get();
+                        foreach ($dt as $p) {
+                            $sub_tot_number = (int) $p->total;
+                            $pajak = 10/100 * $p->total;
+                            $diskon = $t->diskon/100 * $p->total;
+                            $total += $sub_tot_number - $diskon + $pajak + $t->biaya_tambahan;
+                        }
+
+                    }
+                        echo($total);
+                    @endphp
+                </td>
+                <td>
+                    {{ $peng = \App\Pengeluaran::where('id_outlet', $a->id)->sum('harga') }}
+                </td>
+                <td class="d-flex">
+                    @if($total >= $peng)
+                        <span>Untung</span>
+                        <span class="badge bg-success font-12 text-white font-weight-medium badge-pill ml-2 d-lg-block d-md-none">+Rp.{{ $total - $peng }}</span>
+                    @else
+                        <span>Rugi</span>
+                        <span class="badge bg-danger font-12 text-white font-weight-medium badge-pill ml-2 d-lg-block d-md-none">-Rp.{{ $total - $peng }}</span>
+                    @endif
+                </td>
+            </tr>
+
+        </table>
+    </div>
+</div>
+@endforeach
+
+
+@endif
+@if(Auth::user()->level == 'owner')
+<div class="card">
+    <div class="card-body">
+        <div class="d-flex d-lg-flex d-md-block align-items-center">
+            <div>
+                <h3 class="text-dark mb-1 font-weight-medium">Rekapitulasi</h3>
+            </div>
+            <div class="ml-auto mt-md-3 mt-lg-0">
+                <span class="opacity-7 text-muted"><i class="fas fa-hospital-alt"></i></span>
+            </div>
+        </div>
+        <hr>
+        <table>
+            <tr>
+                <td width="100">Pemasukan</td>
+                <td width="100">Pengeluaran</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>
+                    @php
+                    $ts = App\Transaksi::where('id_outlet', Auth::user()->tbUser->id_outlet)->get();
+                    $total = 0;
+                    foreach ($ts as $t) {
+                        $dt = DB::table('tb_detail_transaksi')
+                                ->join('tb_paket', 'tb_detail_transaksi.id_paket', '=', 'tb_paket.id')
+                                ->select(DB::raw('SUM(tb_detail_transaksi.qty * tb_paket.harga) AS total'))
+                                ->where('tb_detail_transaksi.id_transaksi', $t->id)
+                                ->get();
+                        foreach ($dt as $p) {
+                            $sub_tot_number = (int) $p->total;
+                            $pajak = 10/100 * $p->total;
+                            $diskon = $t->diskon/100 * $p->total;
+                            $total += $sub_tot_number - $diskon + $pajak + $t->biaya_tambahan;
+                        }
+
+                    }
+                        echo($total);
+                    @endphp
+                </td>
+                <td>
+                    {{ $peng = \App\Pengeluaran::where('id_outlet', Auth::user()->tbUser->id_outlet)->sum('harga') }}
+                </td>
+                <td class="d-flex">
+                    @if($total >= $peng)
+                        <span>Untung</span>
+                        <span class="badge bg-success font-12 text-white font-weight-medium badge-pill ml-2 d-lg-block d-md-none">+Rp.{{ $total - $peng }}</span>
+                    @else
+                        <span>Rugi</span>
+                        <span class="badge bg-danger font-12 text-white font-weight-medium badge-pill ml-2 d-lg-block d-md-none">-Rp.{{ $total - $peng }}</span>
+                    @endif
+                </td>
+            </tr>
+
+        </table>
+    </div>
+</div>
+@endif
 
 @endsection
